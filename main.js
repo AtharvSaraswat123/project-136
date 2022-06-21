ecae8f01d5931be4249d611ea7490d7054da5243
AtharvSaraswat123 <87174532+AtharvSaraswat123@users.noreply.github.com>
2022-06-21 13:07:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status="";
+video="";
+input_value="";
+function setup(){
+    canvas= createCanvas(480,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(480,380);
+    video.hide()
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+    input_value=document.getElementById("object_name").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+}
+function draw(){
+    image(video,0,0,480,380);
+}
